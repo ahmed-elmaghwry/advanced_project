d9ecbe747d3b460507c9d64c7d47f5db0070a692
ahmed-elmaghwry <ahmed.hassan.elmaghwry@gmail.com>
2025-01-22 08:37:48 +0200

    splashView

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -4,6 +4,7 @@ class AppAssets {
   AppAssets._();
 
   static const String doctorOnboarding = "assets/images/doctor_onboarding.png";
+  static const String splashScreen = "assets/images/splash_screen.png";
   static const String frameBackgroundOnboarding = "assets/svgs/frame_background_onboarding.svg";
   static const String logo = "assets/svgs/logo.svg";
 
